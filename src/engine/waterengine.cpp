b577ec1157c6a51c7ae6b78e96227c13f3aa71c0
Arman Uguray <arman.uguray@gmail.com>
2012-05-07 20:57:14 -0400

    geometric waves work

--- src/engine/waterengine.cpp
+++ src/engine/waterengine.cpp
@@ -11,22 +11,22 @@ extern "C"
 }
 #endif
 
-#define DIM 50
+#define DIM 100
+#define UNIT 2.f
 
 WaterEngine::Wave::Wave() {}
 WaterEngine::Wave::Wave(const WaveParameters &p)
 {
     float wl = p.wavelength;
-    wl = frand() * (2.f * wl - 0.5f * wl) + 0.5f * wl;
+    wl = frand() * (2.f * wl - 0.7f * wl) + 0.7f * wl;
 
-    static float angle = frand() * M_PI_2 - M_PI_4;
+    static float angle = frand() * M_PI - M_PI_2;
     float c = cos(angle);
     float s = sin(angle);
     Vector2 dir(c * p.wave_dir.x - s * p.wave_dir.y,
                 s * p.wave_dir.x + c * p.wave_dir.x);
 
     float st = p.steepness;
-    st = frand() * (2.f * st - 0.5f * st) + 0.5f * st;
 
     params.wavelength = wl;
     params.steepness = st;
@@ -38,11 +38,11 @@ WaterEngine::Wave::Wave(const WaveParameters &p)
 WaterEngine::WaterEngine()
 {
     // initialize parameters
-    m_params.wavelength = 30.f;
+    m_params.wavelength = 16.f;
     m_params.steepness = 0.8f;
-    m_params.kAmpOverLen = 0.2f;
-    m_params.speed = 1.f;
-    m_params.wave_dir = Vector2(-1.f, 1.f).unit();
+    m_params.kAmpOverLen = 0.05f;
+    m_params.speed = 4.f;
+    m_params.wave_dir = Vector2(1.f, 0.5f).unit();
 
     // initialize waves
     initializeWaves();
@@ -53,28 +53,28 @@ WaterEngine::WaterEngine()
     Vector3 *v = &vertices[0];
     float x, z;
     z = -DIM/2.f;
-    for (int i = 0; i < DIM; i++) {
+    for (int i = 0; i < DIM/UNIT; i++) {
         x = -DIM/2.f;
-        for (int j = 0; j < DIM; j++) {
+        for (int j = 0; j < DIM/UNIT; j++) {
             v->x = x;
             v->y = 0.f;
             v->z = z;
             v++;
             v->x = x;
             v->y = 0.f;
-            v->z = z + 1.f;
+            v->z = z + UNIT;
             v++;
-            v->x = x + 1.f;
+            v->x = x + UNIT;
             v->y = 0.f;
-            v->z = z + 1.f;
+            v->z = z + UNIT;
             v++;
-            v->x = x + 1.f;
+            v->x = x + UNIT;
             v->y = 0.f;
             v->z = z;
             v++;
-            x++;
+            x += UNIT;
         }
-        z++;
+        z += UNIT;
     }
 
     glGenBuffers(1, &m_vbo);
@@ -95,27 +95,27 @@ WaterEngine::WaterEngine()
             "   vec2 direction;"
             "};"
 
-            "void wave_function(in Wave waves[4], in float time, in vec3 pos,"
+            "void wave_function(in float waves[24], in float time, in vec3 pos,"
             "                   out vec3 P, out vec3 N, out vec3 B, out vec3 T)"
             "{"
             "   float PI = 3.14159265358979323846264;"
             "   P = pos;"
-            "   for (int i = 0; i < 4; i++) {"
-            "       float A = 16.0 * 0.04;"//waves[i].wavelength * waves[i].kAmpOverLen;"  // Amplitude
-            "       float omega = 2.0 * PI / 16.0;"//waves[i].wavelength;"          // Frequency
-            "       float phi = 4.0 * omega;"//waves[i].speed * omega;"                    // Phase
-            "       float Qi = 0.5/(omega * A * 4.0);"//waves[i].steepness/(omega * A * 4.0);"
+            "   for (int i = 0; i < 24; i += 6) {"
+            "       float A = waves[i] * waves[i+3];"  // Amplitude
+            "       float omega = 2.0 * PI / waves[i];"          // Frequency
+            "       float phi = waves[i+2] * omega;"                    // Phase
+            "       float Qi = waves[i+1]/(omega * A * 4.0);"
 
-            "       float term = omega * dot(normalize(vec2(1.0, 0.5)), vec2(pos.x, pos.z)) + phi * time;"
+            "       float term = omega * dot(vec2(waves[i+4], waves[i+5]), vec2(pos.x, pos.z)) + phi * time;"
             "       float C = cos(term);"
             "       float S = sin(term);"
-            "       P += vec3(Qi * A * 1.0 * C,"
+            "       P += vec3(Qi * A * waves[i+4] * C,"
             "                 A * S,"
-            "                 Qi * A * 0.5 * C);"
+            "                 Qi * A * waves[i+5] * C);"
             "   }"
             "}"
-
-            "uniform Wave waves[4];"
+            
+            "uniform float waves[24];"
             "uniform float time;"
 
             "void main(void)"
@@ -150,7 +150,7 @@ void WaterEngine::render(float elapsed_time)
 {
     m_waveprog->bind();
     m_waveprog->setUniformValue("time", elapsed_time);
-    m_waveprog->setUniformValueArray("waves", (const GLfloat *)m_geo_waves, 4, sizeof(Wave)/sizeof(float));
+    m_waveprog->setUniformValueArray("waves", (const GLfloat *)m_geo_waves, 4 * sizeof(Wave)/sizeof(float), 1);
 
     glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
     glEnableClientState(GL_VERTEX_ARRAY);
